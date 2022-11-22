95501b3a83f2e3b8c179b55cebee0580da77fffa
TM <tm@oxenfree.net>
2022-11-22 15:00:03 +0700

    logo & solutions

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,10 @@
 <template>
   <ul class="flex gap-6 text-center flex-col md:flex-row">
     <li>
-      <a href="#intro" class="active nav-item">Home</a>
+      <a href="#intro" class="nav-item">Home</a>
     </li>
     <li>
       <a href="#contact" class="nav-item call-button">Contact</a>
     </li>
-    <!-- <li class="mt-4 md:mt-0 block md:hidden">
-      <a
-        href="tel:61-401-160-779"
-        target="_blank"
-        class="call-button nav-item pb-1.5"
-      >
-        Call Us</a
-      >
-    </li> -->
   </ul>
 </template>
