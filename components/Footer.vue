2919381eb6496c4291e193a00fc1796015620f08
TM <tm@oxenfree.net>
2023-01-03 18:04:22 +0700

    add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,87 @@
 <template>
   <div class="w-screen bg-background">
-    <footer>
-      <div class="flex flex-row gap-2 items-center h-full">
+    <footer class="container">
+      <div class="grid grid-cols-2 pb-8">
+        <div class="flex flex-col justify-center">
+          <h5 class="uppercase text-primary font-bold mb-4">Membership</h5>
+          <div class="flex items-center">
+            <img
+              src="/icons/FBAA.png"
+              alt="FBAA"
+              class="h-8 w-auto ml-auto mb-4"
+            />
+            <img
+              src="/icons/afg_2021_rgb_reversed_sm.png"
+              alt="AFG"
+              class="h-6 w-auto mx-auto mb-4"
+            />
+            <img
+              src="/icons/AFCA_logo_white_lores.png"
+              alt="AFCA"
+              class="h-10 w-auto mr-auto mb-4"
+            />
+          </div>
+        </div>
+        <div>
+          <h5 class="uppercase text-primary font-bold mb-4">Follow Us</h5>
+          <div class="flex gap-4 justify-center">
+            <a
+              href="https://www.facebook.com/MJO-Mortgage-Solutions-105146435792036"
+              target="_blank"
+              alt="Facebook"
+            >
+              <svg
+                class="sosmed"
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://www.instagram.com/mjo_mortgage_solutions/"
+              target="_blank"
+              alt="Instagram"
+            >
+              <svg
+                class="sosmed"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24px"
+                height="24px"
+                viewBox="0 0 260 260"
+              >
+                <path
+                  d="M164.58,64H95.42C78.1,64,64,78.1,64,95.42v69.16C64,181.9,78.1,196,95.42,196h69.16c17.32,0,31.42-14.1,31.42-31.42V95.42 C196,78.1,181.9,64,164.58,64z M130,171.1c-22.66,0-41.1-18.44-41.1-41.1s18.44-41.1,41.1-41.1s41.1,18.44,41.1,41.1 S152.66,171.1,130,171.1z M172.22,97.3c-5.3,0-9.6-4.3-9.6-9.61c0-5.3,4.3-9.6,9.6-9.6c5.31,0,9.61,4.3,9.61,9.6 C181.83,93,177.53,97.3,172.22,97.3z M130,102.9c-14.94,0-27.1,12.16-27.1,27.1s12.16,27.1,27.1,27.1s27.1-12.16,27.1-27.1 S144.94,102.9,130,102.9z M130,2C59.31,2,2,59.31,2,130s57.31,128,128,128s128-57.31,128-128S200.69,2,130,2z M210,164.58 c0,25.04-20.38,45.42-45.42,45.42H95.42C70.38,210,50,189.62,50,164.58V95.42C50,70.38,70.38,50,95.42,50h69.16 C189.62,50,210,70.38,210,95.42V164.58z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p class="text-xs font-light text-primary text-center">
+          We acknowledge the Traditional Owners of the land where I/we work and
+          live. We pay our respects to Elders past, present and emerging. We
+          celebrate the stories, culture and traditions of Aboriginal and Torres
+          Strait Islander Elders of all communities who also work and live on
+          this land. ABN: 61 921 877 088 |Credit Representative Number: 541 627
+          | Website by General Technical 2022
+        </p>
+      </div>
+      <div class="flex flex-row gap-2 items-center h-full justify-center py-8">
+        <p class="text-xs text-primary border-r-2 pr-2">
+          <span class="font-bold">ABN: </span>
+          61 921 877 088
+        </p>
+        <p class="text-xs text-primary border-r-2 pr-2">
+          <span class="font-bold">Credit Representative Number: </span>
+          541 627
+        </p>
         <p class="font-bold text-xs text-primary">
           &copy; <span class="font-bold">2022</span>
         </p>
@@ -20,3 +100,6 @@
     </footer>
   </div>
 </template>
+
+ABN: 61 921 877 088 |Credit Representative Number: 541 627 | Website by General
+Technical 2022
