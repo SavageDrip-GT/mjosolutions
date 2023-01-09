4eea5898f988e2b8e293207c34be199317c0baef
TM <tm@oxenfree.net>
2023-01-09 14:40:34 +0700

    add lenders

--- pages/faq.vue
+++ pages/faq.vue
@@ -6,67 +6,67 @@
         <div class="grid grid-cols-1 md:grid-cols-2 md:gap-8">
           <div>
             <h4>What is a mortgage broker?</h4>
-            <h6>
+            <p>
               A mortgage broker acts as a middleman that connects you to a
               lender. They evaluate your financial situation and capacity to
               borrow before presenting you with a selection of possible home
               loans that best suits your needs.
-            </h6>
+            </p>
             <h4>Why use a mortgage broker?</h4>
-            <h6>
+            <p>
               A Mortgage Broker can access hundreds of options from over 25
               banks and lenders and negotiate on your behalf to get you the best
               rate possible. Banks can only offer their products.
-            </h6>
+            </p>
             <h4>How much does a mortgage broker cost?</h4>
-            <h6>Nothing! It's a free service.</h6>
-            <h6>
+            <p>Nothing! It's a free service.</p>
+            <p>
               From initial discussions through to the settlement, a Mortgage
               Broker's services are free!
-            </h6>
+            </p>
             <h4>How do Mortgage Brokers get paid?</h4>
-            <h6>
+            <p>
               Mortgage Brokers receive a commission from the bank for bringing
               your business to them.
-            </h6>
+            </p>
             <h4>Are you available after hours and weekends?</h4>
-            <h6>Yes!</h6>
-            <h6>
+            <p>Yes!</p>
+            <p>
               Being a mobile brokerage, We can connect with you at a time and
               location most convenient to you inside and outside of regular
               business hours.
-            </h6>
+            </p>
             <h4>How much can I borrow?</h4>
-            <h6>
+            <p>
               Your borrowing capacity will be assessed by the lender,
               considering your current income, employment status, credit history
               and levels of personal debt.
-            </h6>
+            </p>
           </div>
           <div>
             <h4>What is an offset account?</h4>
-            <h6>
+            <p>
               An offset account is an everyday bank account linked to your home
               loan. You can deposit your salary and savings into the account,
               and the balance is then offset against the amount owing on your
               home loan.
-            </h6>
-            <h6>
+            </p>
+            <p>
               Say you have a home loan of $500,000 and $50,000 in your offset
               account; in this situation, you'll only be charged interest on a
               loan balance of $450,000 ($500,000 - $50,000).
-            </h6>
-            <h6>
+            </p>
+            <p>
               Because the offset account acts like an everyday account, your
               $50,000 is still accessible whenever you need it, even while it's
               working to reduce your overall interest payments.
-            </h6>
-            <h6>
+            </p>
+            <p>
               The advantages of an offset account depend on how much money you
               have sitting in it and the type of offset you have.
-            </h6>
+            </p>
             <h4>Should I go with a Fixed or Variable Intrest Rate?</h4>
-            <h6>
+            <p>
               Fixed-rate home loans are outstanding for locking in a low rate
               and can be ideal for borrowers trying to stick to a set budget.
               However, fixed-rate home loans aren't always as flexible as
@@ -75,16 +75,16 @@
               features (such as a redraw facility or offset account). That's why
               it's always a good idea to carefully assess the pros and cons of
               both options before you decide.
-            </h6>
+            </p>
             <h4>What is LMI?</h4>
-            <h6>
+            <p>
               LMI is an insurance policy designed to protect the lender if the
               borrower defaults on their mortgage. Most lenders will include LMI
               as a standard requirement when the borrower's deposit is less than
               20% of the property purchase price. LMI costs vary, and it can add
               a high cost to your total purchase price. LMI is payable as a
               once-off premium that gets added to your entire home loan amount.
-            </h6>
+            </p>
           </div>
         </div>
         <!-- <ContentDoc path="/faq" v-slot="{ doc }">
--- pages/lenders.vue
+++ pages/lenders.vue
@@ -2,12 +2,16 @@
   <NuxtLayout>
     <section id="faq" class="min-h-screen py-24 sm:py-36">
       <FadeUp class="container">
-        <h3 class="text-primary text-center mb-8">
+        <h2 class="text-primary text-center">
           Why talk to 40 different banks when you can talk to the team at MJO
-          Mortgage Solutions. Let us do the talking and find the right bank for
-          you
-        </h3>
-        <div class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-6">
+          Mortgage Solutions.
+        </h2>
+        <h4 class="mb-8 text-center text-white">
+          Let us do the talking and find the right bank for you.
+        </h4>
+        <div
+          class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-2 sm:gap-3"
+        >
           <div
             :data="lenders"
             v-for="lender of lenders"
